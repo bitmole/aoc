e26b5acc5683ba7811deb897f9ac9625205c591b
bitmole <robert@idea-loop.com>
2024-01-03 17:25:05 -0700

    Sum powers of cubes, completing day puzzle.

--- 2023/day_2/cubes.py
+++ 2023/day_2/cubes.py
@@ -129,3 +129,20 @@ def sum_possible_games():
         if ok:
             total += (i + 1)
     return total
+
+def sum_cube_powers(games):
+    total = 0
+    for game in games:
+        r, g, b = max_color_counts(game)
+        total += r * g * b
+    return total
+
+
+def test_cube_powers():
+    test_input = '''Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'''
+    assert sum_cube_powers(test_input.splitlines()) == 2286
+    print('okay')
