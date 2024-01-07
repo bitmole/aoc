ecdb2b3346a3309c5ac648e404190eb50546c63a
bitmole <robert@idea-loop.com>
2024-01-07 13:18:38 -0700

    Extract numbers and their positions

--- 2023/day_3/schematic.py
+++ 2023/day_3/schematic.py
@@ -1,4 +1,5 @@
 import unittest
+import re
 
 def parse_matrix(rows):
     matrix = []
@@ -8,6 +9,13 @@ def parse_matrix(rows):
             row.append(c)
         matrix.append(row)
     return matrix
+
+def extract_numbers(matrix):
+    numbers = []
+    for r in matrix:
+        for m in re.finditer(r'(\d+)', ''.join(r)):
+            numbers.append((m.group(0), m.start(), m.end()))
+    return numbers
 
 class KnownValues(unittest.TestCase):
     test_input = '''467..114..
@@ -26,6 +34,13 @@ class KnownValues(unittest.TestCase):
         self.assertEqual(len(matrix), 10) 
         self.assertEqual(len(matrix[0]), 10) 
         self.assertEqual(matrix[0][2], '7') 
+
+    def test_extract_numbers(self):
+        matrix = parse_matrix(self.test_input.splitlines())
+        numbers = extract_numbers(matrix)
+        self.assertEqual(numbers[0], ('467', 0, 3))
+        self.assertEqual(numbers[1], ('114', 5, 8))
+        self.assertEqual(numbers[2], ('35', 2, 4))
 
 if __name__ == "__main__":
     unittest.main()
