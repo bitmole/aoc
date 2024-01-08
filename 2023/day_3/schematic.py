aca28b9a314ecd2e74dcfda6ac5aa60a3c6f780b
bitmole <robert@idea-loop.com>
2024-01-08 11:05:01 -0700

    List adjacent cells in schematic matrix.

--- 2023/day_3/schematic.py
+++ 2023/day_3/schematic.py
@@ -15,12 +15,46 @@ def extract_numbers(matrix):
     for i, r in enumerate(matrix):
         for m in re.finditer(r'(\d+)', ''.join(r)):
             n, s, e = m.group(0), m.start(), m.end()
-            is_last_col = e == len(r)
-            numbers.append((n, s, e, i, is_last_col))
+            numbers.append((n, s, e, i))
     return numbers
 
-def list_adjacents(numbers):
-    pass
+def list_adj_cells(n_rec, matrix):
+    cells = []
+    n, start, end, i = n_rec
+    has_prev_row = i > 0
+    has_next_row = i < len(matrix) - 1
+    has_prev_col = start > 0
+    has_next_col = len(matrix[i]) > end
+
+    # above
+    if has_prev_row:
+        above = matrix[i-1]
+        cells += above[start:end]
+        print('above', cells)
+
+    # below
+    if has_next_row:
+        below = matrix[i+1]
+        cells += below[start:end]
+        print('below', cells)
+
+    # left
+    if has_prev_col:
+        left = [r[start-1] for r in matrix]
+        s = i-1 if has_prev_row else i
+        e = i+2
+        cells += left[s:e]
+        print('left', cells)
+
+    # right
+    if has_next_col:
+        right = [r[end] for r in matrix]
+        s = i-1 if has_prev_row else i
+        e = i+2
+        cells += right[s:e]
+        print('right', cells)
+        
+    return cells
 
 class KnownValues(unittest.TestCase):
     test_input = '''467..114..
@@ -43,10 +77,17 @@ class KnownValues(unittest.TestCase):
     def test_extract_numbers(self):
         matrix = parse_matrix(self.test_input.splitlines())
         numbers = extract_numbers(matrix)
-        self.assertEqual(numbers[0], ('467', 0, 3, 0, False))
-        self.assertEqual(numbers[1], ('114', 5, 8, 0, False))
-        self.assertEqual(numbers[2], ('35', 2, 4, 2, False))
-        self.assertEqual(numbers[7], ('755', 6, 9, 7, False))
+        self.assertEqual(numbers[0], ('467', 0, 3, 0))
+        self.assertEqual(numbers[1], ('114', 5, 8, 0))
+        self.assertEqual(numbers[2], ('35', 2, 4, 2))
+        self.assertEqual(numbers[7], ('755', 6, 9, 7))
+
+    def test_list_adj_cells(self):
+        matrix = parse_matrix(self.test_input.splitlines())
+        numbers = extract_numbers(matrix)
+        n = numbers[6]
+        cells = list_adj_cells(n, matrix)
+        self.assertEqual(cells, ['.', '.', '.', '.', '.', '.','.', '.', '.', '+', '.', '.'])
 
 if __name__ == "__main__":
     unittest.main()
