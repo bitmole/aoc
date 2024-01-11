58433b6c4e67261691d4e0399d581438751bd965
bitmole <robert@idea-loop.com>
2024-01-11 15:39:22 -0700

    Simplify doubling of successive winning numbers.

--- 2023/day_4/scratchcards.py
+++ 2023/day_4/scratchcards.py
@@ -1,13 +1,14 @@
 import re
 
 def card_worth(card):
-    print(card)
     winning, hand = card.split('|')
     winning = re.findall('\d+', winning)[1:] # throw away card number
     hand = re.findall('\d+', hand)
-    worth = 0
-    for n in hand:
-        if n in winning:
-            worth = 1 if worth==0 else 2*worth
 
-    return worth
+    # holding any winning numbers?
+    winning_in_hand = set(winning).intersection(set(hand))
+    if winning_in_hand: 
+        exp = len(winning_in_hand) - 1
+        return pow(2, exp)
+    else: 
+        return 0
